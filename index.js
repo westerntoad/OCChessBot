350fcfd5758b67e5aef7fa0a72287f0cc096e90e
westerntoad <91150531+westerntoad@users.noreply.github.com>
2022-01-26 21:05:30 -0800

    Added /shutdown and welcome reiteration
    Messing about

--- index.js
+++ index.js
@@ -9,16 +9,45 @@ const client = new DiscordJS.Client({
     ]
 })
 
+const channel = client.channels.cache.get('919369946855784455')
+
 client.on('ready', () => {
-    console.log('the bot is ready')
+    console.log('Loading...')
+
+    // guild
+    global.occc = client.guilds.cache.get('919369946855784448')
+
+    // channels
+    global.welcome = client.channels.cache.get('919369946855784451') // welcome channel
+    global.test = client.channels.cache.get('936107832275775548')    // bot-testing channel
+    
+    // permission levels
+    global.officers = global.occc.roles.cache.get('919369946855784449') // officer
+    global.advisor = global.occc.roles.cache.get('934005868800921630') // advisor
+    console.log('The bot is ready.')
 })
 
 client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
-        })
-    }
-})
+    if(!message.author.bot) {
+        // not a bot
+        if(message.channel == global.welcome) {
+            global.welcome.send(message.content)
+            message.delete()
+        }
 
-client.login(process.env.TOKEN)
+        if(message.member.roles.cache.has(global.officers.id) || message.member.roles.cache.has(global.advisor.id)) {
+            // officer
+            if(message.content == '/shutdown') {
+                //test.send('Shutting down...')
+                client.destroy()
+            }
+
+            if(message.member.roles.cache.has(global.advisor.id)) {
+                // advisor
+
+            }
+        }
+    }
+}
+
+,client.login(process.env.TOKEN))
