795d0854b4fb09e5e36d956f49a71879b032641f
westerntoad <91150531+westerntoad@users.noreply.github.com>
2022-01-28 02:01:44 -0800

    Just got more confused...

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import DiscordJS, { Client, Intents } from 'discord.js'
+import DiscordJS, { Client, Intents, Permissions } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 console.log("Bot starting")
@@ -16,59 +16,63 @@ client.on('ready', () => {
 
     client.user.setActivity("Chess", { type: "COMPETING"})
 
-    // guild
-    global.occc = client.guilds.cache.get('919369946855784448')
+    // application commands
+    client.api.applications(client.user.id).guilds(process.env.GUILD_ID).commands.post({data: {
+        name: 'ping',
+        description: 'ping pong!'
+    }})
 
-    // channels
-    global.welcome = client.channels.cache.get('919369946855784451') // welcome channel
-    global.alert = client.channels.cache.get('936126646145130506')    // alerts channel
-    global.test = client.channels.cache.get('936107832275775548')    // bot-testing channel
-    
-    // permission levels
-    global.officers = global.occc.roles.cache.get('919369946855784449') // officer
-    global.advisor = global.occc.roles.cache.get('934005868800921630') // advisor
-    console.log('Bot readied at ' + global.startTime.toLocaleTimeString() + '.')
-    global.alert.send('Bot started at ' + global.startTime.toLocaleTimeString() + '.')
+    console.log('Bot readied at ' + global.startTime.toLocaleString() + '.')
 })
 
 function shutdown() { // todo Time ran for
-    console.log('Shut down at ' + new Date().toLocaleTimeString() + '.\nBot ran for ' + NaN)
-    global.alert.send('Shut down at ' + new Date().toLocaleTimeString() + '.\nBot ran for ' + NaN).then(m => {
-        client.destroy()
-        process.exit(1)
-      })
+    console.log('Shut down at ' + new Date().toLocaleString() + '.\nRan for ' + NaN)
+    client.destroy()
+    process.exit(1)
 }
 
 function restart() {
-    console.log('Restarted at ' + new Date().toLocaleTimeString() + '.')
-    global.alert.send('Restarted at ' + new Date().toLocaleTimeString() + '.').then(m => {
-        client.destroy()
-        client.login(process.env.TOKEN)
-      })
+    console.log('Restarted at ' + new Date().toLocaleString() + '.')
+    client.destroy()
+    client.login(process.env.TOKEN)
+}
+
+function alert(text) {
+    try {
+        client.channels.cache.get(process.env.C_ALERT).send(text)
+    } catch(err) {
+        console.log('ERROR: Cannot send alert.')
+    }
 }
 
 client.on('messageCreate', (message) => {
     if(message.author.bot)
-        return // not a bot
+        return // anyone not a bot
     
-        if(message.channel == global.welcome) {
-        global.welcome.send(message.content)
-        message.delete()
-    }
-
-    if(message.member.roles.cache.has(global.officers.id) || message.member.roles.cache.has(global.advisor.id)) {
-        // officer
-        if(message.content == '/shutdown') {
-            shutdown()
-        } else if (message.content == '/restart') {
+    
+    
+    if(message.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
+        // can ban and kick
+        if (message.content == '/restart')
             restart()
-        }
 
-        if(message.member.roles.cache.has(global.advisor.id)) {
-            // advisor
 
+        if(message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+            // admin
+            //if(message.content == '/shutdown')
+                //shutdown()
         }
     }
-}
+})
 
-,client.login(process.env.TOKEN))
+client.ws.on('INTERACTION_CREATE', async interaction => {
+    client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+        type: 4,
+        data: {
+          content: 'hello world!'
+        }
+    }})
+    //shutdown()
+})
+
+client.login(process.env.TOKEN)
